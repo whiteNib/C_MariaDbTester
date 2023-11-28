aa27e3fdb4d3ddd3ff83cf5cbc64d4e2d775e84a
whiteNib <139529654+whiteNib@users.noreply.github.com>
2023-11-28 22:25:58 +0900

    Add files via upload

---
+++ MariaDbTester/MariaDbTester/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MariaDbTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(394, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "확인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(72, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(419, 350);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(505, 417);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(269, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "analyzer tag";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "house";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "measure type";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ MariaDbTester/MariaDbTester/Form1.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MariaDbTester
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection = new MySqlConnection(string.Format("Server={0};Database={1};Uid ={2};Pwd={3};",
+"127.0.0.1", "testdb", "root", "123456"));
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            if (textBox1.Text == "")
+            {
+                string selectQuery = "SELECT * from testdb";
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(selectQuery, connection);
+                MySqlDataReader table = cmd.ExecuteReader();
+
+                while(table.Read()) 
+                {
+                    dataGridView1.Rows.Add(table["analyzer_tag"], table["house_tag"], table["measure_type"]);
+                }
+                connection.Close();
+            }
+            else
+            {
+                string selectQuery = "SELECT * from testdb where analyzer_tag" + textBox1.Text + "";
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(selectQuery, connection);
+                MySqlDataReader table = cmd.ExecuteReader();
+
+                ArrayList Test = new ArrayList();
+                while(table.Read()) 
+                {
+                    Test.Add(table["analyzer_tag"]);
+                    dataGridView1.Rows.Add(table["analyzer_tag"], table["house_tag"], table["measure_type"]);
+                }
+
+                textBox2.Text = Test[1].ToString();
+                connection.Close();
+            }
+        }
+    }
+}
